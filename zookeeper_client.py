8a9bd6a7772d250e58d6126bd74cbdd1e3332352
unknown <1156609896@qq.com>
2021-05-10 20:36:48 +0800

    'v0.3'

--- zookeeper_client.py
+++ zookeeper_client.py
@@ -1,17 +1,16 @@
 # -*- coding: UTF-8 -*-
 import logging
 import sys
-from time import sleep
+from threading import Condition
 
 from kazoo.client import KazooClient
 
 hosts = '127.0.0.1:2181,127.0.0.1:2182,127.0.0.1:2183'
 logging.basicConfig(level=logging.WARNING, stream=sys.stdout)
-
 # 创建一个客户端，可以指定多台zookeeper，
 zk = KazooClient(hosts=hosts, logger=logging)
 zk.start()
-server_list = ["minisql1", "minisql2", "minisql"]
+server_list = ["minisql1", "minisql2", "minisql3"]
 
 
 def cmd_get_sql():
@@ -31,18 +30,25 @@ def set_sql(target_path_list, sql):
         zk.set(target, sql)
 
 
-@zk.DataWatch("/servers/minisql1/result")  # 当节点kazoo的数据变化时这个函数会被调用
+condition = Condition()
+
+
+# 当节点kazoo的数据变化时这个函数会被调用
+# 如果节点被删除这个函数也会被调用，但是data和stat都是None
 def watch_instruction_node(data, stat):
-    # 如果节点被删除这个函数也会被调用，但是data和stat都是None
     if stat and data:
         data_str = data.decode("utf-8")
         print("Result: \n" + data_str)
     else:
         print("节点未初始化或已被删除")
-    sql = cmd_get_sql()
-    set_sql(["/servers/minisql1/instruction"], bytes(sql, encoding="utf-8"))
+
+    condition.notify()
 
 
 if __name__ == '__main__':
+    for server in server_list:
+        zk.DataWatch("/servers/" + server + "/result", watch_instruction_node)
     while True:
-        sleep(600)
+        sql = cmd_get_sql()
+        set_sql(["/servers/minisql1/instruction"], bytes(sql, encoding="utf-8"))
+        semaphore.acquire()
