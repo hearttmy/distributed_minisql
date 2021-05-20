c17a0929af972f1961ed138f70bdfdf5a3f2b3b2
unknown <1156609896@qq.com>
2021-05-20 11:29:39 +0800

    'v0.3'

--- minisql_cluster/src/main.py
+++ minisql_cluster/src/main.py
@@ -7,32 +7,38 @@ from kazoo.client import KazooClient
 from interpreter import parser, zookeeper_result
 
 hosts = '172.16.238.2:2181,172.16.238.3:2182,172.16.238.4:2183'
+test_hosts = '127.0.0.1:2181,127.0.0.1:2182,127.0.0.1:2183'
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 server_path = "/servers/" + sys.argv[1]
-
 # 创建一个客户端，可以指定多台zookeeper，
-zk = KazooClient(hosts=hosts, logger=logging)
-flag = 0
+zk = KazooClient(hosts=test_hosts, logger=logging)
 
-@zk.DataWatch(server_path + "/instruction")  # 当节点kazoo的数据变化时这个函数会被调用
-def watch_instruction_node(data, stat):
-    global flag
-    # 如果节点被删除这个函数也会被调用，但是data和stat都是None
-    if stat and data and flag:
-        data_str = data.decode("utf-8")
-        print("Version: %s, data: %s" % (stat.version, data_str))
-        parser.parse(data_str)
-        zookeeper_result(zk, server_path + "/result")
-    else:
-        flag = 1
-        print("节点未初始化或已被删除")
+
+def watch_instruction_children(children):
+    print(children)
+    for ch in children:
+        data, stat = zk.get('{}/instructions/{}'.format(server_path, ch))
+        if data and stat:
+            data_str = data.decode('utf-8')
+            parser.parse(data_str)
+            zk.ensure_path('{}/instructions/{}/result'.format(server_path, ch))
+            zookeeper_result(zk, '{}/instructions/{}/result'.format(server_path, ch))
 
 
 if __name__ == '__main__':
     # 开始心跳
     zk.start()
-    zk.ensure_path(server_path + "/result")
-    zk.ensure_path(server_path + "/instruction")
+    zk.ensure_path('/tables')
+    zk.ensure_path("{}/tables".format(server_path))
+    zk.ensure_path("{}/info".format(server_path))
+    if not zk.exists("{}/info/recordNum".format(server_path)):
+        zk.create("{}/info/recordNum".format(server_path), b'0')
+    if not zk.exists("{}/info/tableNum".format(server_path)):
+        zk.create("{}/info/tableNum".format(server_path), b'0')
+    zk.delete("{}/instructions".format(server_path), recursive=True)
+    zk.ensure_path("{}/instructions".format(server_path))
+    zk.ChildrenWatch(server_path + "/instructions", watch_instruction_children)
+
     while True:
         sleep(60)
         print("Watching...")
--- zookeeper_client.py
+++ zookeeper_client.py
@@ -1,6 +1,9 @@
 # -*- coding: UTF-8 -*-
+import hashlib
 import logging
+import random
 import sys
+import time
 from threading import Condition
 
 from kazoo.client import KazooClient
@@ -9,8 +12,9 @@ hosts = '127.0.0.1:2181,127.0.0.1:2182,127.0.0.1:2183'
 logging.basicConfig(level=logging.WARNING, stream=sys.stdout)
 # 创建一个客户端，可以指定多台zookeeper，
 zk = KazooClient(hosts=hosts, logger=logging)
-zk.start()
 server_list = ["minisql1", "minisql2", "minisql3"]
+condition = Condition()
+dataWatchFinished = 1
 
 
 def cmd_get_sql():
@@ -25,30 +29,88 @@ def cmd_get_sql():
             s = input()
 
 
-def set_sql(target_path_list, sql):
-    for target in target_path_list:
-        zk.set(target, sql)
+def get_min_records():
+    tmp = []
+    for server in server_list:
+        tmp.append(zk.get("/servers/{}/info/recordNum".format(server)))
 
 
-condition = Condition()
+def get_min_tables():
+    tmp = []
+    for server in server_list:
+        tmp.append(zk.get("/servers/{}/info/tableNum".format(server)))
+
+
+def get_target_server(sql):
+    tmp = sql.lstrip(' ')
+    ans = ['minisql1']
+    if tmp[0] == 'create' and tmp[1] == 'table':  # backup
+        pass
+    elif tmp[0] == 'select':  # balancing
+        pass
+    else:
+        pass
+    return ans
+
+
+def get_path_list(target_server, sql):
+    ans = []
+    m = hashlib.sha256()
+    for server in target_server:
+        m.update((str(random.random()) + sql + str(time.time())).encode('utf-8'))
+        node_name = m.hexdigest()
+        ans.append('/servers/{}/instructions/{}'.format(server, node_name))
+    return ans
+
+
+def deleteFinishedNode(path_list):
+    for path in path_list:
+        if zk.exists(path):
+            zk.delete(path, recursive=True)
+
+
+def set_sql_and_watchers(path_list, sql):
+    for path in path_list:
+        zk.create(path, sql)
+        zk.ensure_path(path + '/result')
+        zk.DataWatch(path + '/result', watch_result_node)
 
 
 # 当节点kazoo的数据变化时这个函数会被调用
 # 如果节点被删除这个函数也会被调用，但是data和stat都是None
-def watch_instruction_node(data, stat):
+def watch_result_node(data, stat):
+    global dataWatchFinished
+    condition.acquire()
+
     if stat and data:
+        dataWatchFinished += 1
         data_str = data.decode("utf-8")
         print("Result: \n" + data_str)
-    else:
-        print("节点未初始化或已被删除")
 
     condition.notify()
+    condition.release()
 
 
 if __name__ == '__main__':
-    for server in server_list:
-        zk.DataWatch("/servers/" + server + "/result", watch_instruction_node)
+    m = hashlib.sha256()
+    zk.start()
+    target_server = []
+    path_list = []
+    node_name = 'test'
     while True:
+        condition.acquire()
+        while dataWatchFinished != 1:
+            condition.wait()
+
+        deleteFinishedNode(path_list)
+
         sql = cmd_get_sql()
-        set_sql(["/servers/minisql1/instruction"], bytes(sql, encoding="utf-8"))
-        semaphore.acquire()
+        target_server = get_target_server(sql)
+        path_list = get_path_list(target_server, sql)
+        print(path_list)
+
+        # quit and file exec
+        set_sql_and_watchers(path_list, bytes(sql, encoding="utf-8"))
+
+        dataWatchFinished = 0
+        condition.release()
